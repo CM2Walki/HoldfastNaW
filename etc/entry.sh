242a816c8b6f0cb92b5e6002f9c35a2e6fd79052
Walentin <walentinlamonos@googlemail.com>
2022-12-06 15:11:48 +0100

    Update entry.sh
    Removed obsolete STEAM_COM_PORT

--- etc/entry.sh
+++ etc/entry.sh
@@ -13,7 +13,6 @@ fi
 # Replace pre-set variables
 sed -i -e 's/server_port 20100/'"server_port ${SERVER_PORT}"'/g' \
 		-e 's/server_region europe/'"server_region ${SERVER_REGION}"'/g' \
-		-e 's/steam_communications_port 8700/'"steam_communications_port ${STEAM_COM_PORT}"'/g' \
 		-e 's/steam_query_port 27000/'"steam_query_port ${STEAM_QUERY_PORT}"'/g' \
 			"${STEAMAPPDIR}/${SERVER_CONFIG_PATH}"
 
